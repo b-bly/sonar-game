78facff7cec90d1bfabc891d3e8f84867d193239
b-bly <brendt.bly@outlook.com>
2025-01-22 06:16:10 -0600

    make move.

--- board.py
+++ board.py
@@ -1,73 +1,103 @@
-# Writing game described at 
+# Writing game described at
 # https://inventwithpython.com/invent4thed/chapter13.html
 # Using object oriented and functional principals instead of
 # just functions
 
 import random
-
+import math
 
 FOUR_SPACES = '    '
 THREE_SPACES = '   '
 
+
 class Board:
-  def __init__(self, height=15, width=60, numChests=3):
-    self.height = height
-    self.width = width
-    self.widthTens = round(width/10)
-    self.numChests = numChests
+    def __init__(self, y=15, x=60, numChests=3):
+        self.y = y
+        self.x = x
+        self.xTens = round(x/10)
+        self.numChests = numChests
+        self.chests = []
+        self.board = []
 
-  def getNewBoard(self):
-  # Create a width x height board.
-    board = []
-    for x in range(self.width):
-      board.append([])
-      for y in range(self.height):
-        board[x].append("~")
-    return board
-  
-  def drawBoard(self, board):
-    self.drawXTens()
-    self.drawXOnes()
-    print()
-    self.drawBoardMiddle(board)
-    print()
-    self.drawXOnes()
-    self.drawXTens()
+    def getNewBoard(self):
+        # Create a x x y board.
+        # TODO Save board in this class?
+        board = []
+        for x in range(self.x):
+            board.append([])
+            for y in range(self.y):
+                board[x].append("~")
+        self.board = board
+        return board
 
-  def drawXTens(self):
-    xTens = FOUR_SPACES
-    for i in range(1, self.widthTens):
-      xTens += (' ' * 9) + str(i)
-    print(xTens)
+    def drawBoard(self):
+        board = self.board
+        self.drawXTens()
+        self.drawXOnes()
+        print()
+        self.drawBoardMiddle(board)
+        print()
+        self.drawXOnes()
+        self.drawXTens()
 
-  def drawXOnes(self):
-    xOnes = THREE_SPACES + ('0123456789' * self.widthTens)
-    print(xOnes)
+    def drawXTens(self):
+        xTens = FOUR_SPACES
+        for i in range(1, self.xTens):
+            xTens += (' ' * 9) + str(i)
+        print(xTens)
 
-  def drawBoardMiddle(self, board):
-    for row in range(self.height):
-      extraSpace = ''
-      if row < 10:
-          extraSpace = ' '
-      boardRow = ''
-      for column in range(self.width):
-        boardRow += board[column][row]
-      print('%s%s %s %s' % (extraSpace, row, boardRow, row))
+    def drawXOnes(self):
+        xOnes = THREE_SPACES + ('0123456789' * self.xTens)
+        print(xOnes)
 
-  def getRandomChests(self):
-     # Create a list of chest data structures (two-item lists of x, y int
-     chests = []
-     while len(chests) < self.numChests:
-         newChest = [random.randint(0, self.width - 1), random.randint(0, self.height)]
-         if newChest not in chests:
-             chests.append(newChest)
-     return chests
- 
-  def isOnBoard(self, x, y):
-    return x >= 0 and x <= self.width and y >= 0 and y <= self.height
+    def drawBoardMiddle(self):
+        board = self.board
+        for row in range(self.y):
+            extraSpace = ''
+            if row < 10:
+                extraSpace = ' '
+            boardRow = ''
+            for column in range(self.x):
+                boardRow += board[column][row]
+            print('%s%s %s %s' % (extraSpace, row, boardRow, row))
+
+    def getRandomChests(self):
+        # Create a list of chest data structures (two-item lists of x, y int
+        chests = []
+        while len(chests) < self.numChests:
+            newChest = [random.randint(0, self.x - 1),
+                        random.randint(0, self.y)]
+            if newChest not in chests:
+                chests.append(newChest)
+        self.chests = chests
+        return chests
+
+    def isOnBoard(self, x, y):
+        return x >= 0 and x <= self.x and y >= 0 and y <= self.y
+
+    def makeMove(self, x, y):
+        board = self.board
+        chests = self.chests
+        smallestDistance = 100  # Any chest will be closer than 100.
+        for cx, cy in chests:
+            distance = math.sqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
+            if distance < smallestDistance:  # We want the closest treasure chest
+                smallestDistance = distance
+            smallestDistance = round(smallestDistance)
+            if smallestDistance == 0:
+                # xy is directly on a treasure chest!
+                chests.remove([x, y])
+                return 'You have found a sunken treasure chest!'
+            else:
+                if smallestDistance < 10:
+                    board[x][y] = str(smallestDistance)
+                    return 'Treasure detected at a distance of %s from the sonar device.' % (smallestDistance)
+                else:
+                    board[x][y] = 'X'
+                    return 'Sonar did not detect anything. All treasure chests out of range.'
+
 
 board = Board()
 theBoard = board.getNewBoard()
 board.drawBoard(theBoard)
 print(board.getRandomChests())
-
